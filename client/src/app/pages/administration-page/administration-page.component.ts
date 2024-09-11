5730b9ddd739a593bfa52d7de2299f3490602f85
Kfor23 <kimia.foroudian@polymtl.ca>
2024-09-11 13:37:22 -0400

    feat(admin): Initial layout of the admin page

---
+++ client/src/app/pages/administration-page/administration-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// TODO : Avoir un fichier séparé pour les constantes!
+
+@Component({
+    selector: 'app-administration-page',
+    standalone: true,
+    templateUrl: './administration-page.component.html',
+    styleUrls: ['./administration-page.component.scss'],
+})
+export class AdministrationPageComponent {}
--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,7 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter } from '@angular/router';
+import { AdministrationPageComponent } from '@app/pages/administration-page/administration-page.component';
 import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
@@ -13,15 +14,15 @@ if (environment.production) {
     enableProdMode();
 }
 
-
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'administration', component: AdministrationPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
 bootstrapApplication(AppComponent, {
     providers: [provideHttpClient(), provideRouter(routes), provideAnimations()],
-})
+});
