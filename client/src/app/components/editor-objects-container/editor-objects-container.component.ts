02a4841944a7ac5f2e10389437da2f320a9a7a7d
KimDesroches <kim.desroches@polymtl.ca>
2024-09-13 16:48:37 -0400

    feat(map-creation-page): Display objects container, map size, map title and map description * Change count display according to map size * Display description when object is hovered

---
+++ client/src/app/components/editor-objects-container/editor-objects-container.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+    selector: 'app-editor-objects-container',
+    standalone: true,
+    templateUrl: './editor-objects-container.component.html',
+    styleUrls: ['./editor-objects-container.component.scss'],
+    imports: [FormsModule, RouterLink],
+})
+export class EditorObjectsContainerComponent {
+    gameObjects = [
+        { id: 'item-1', name:'Trident de Poséidon', image: 'assets/images/objects/poseidon-trident.jpg', description: 'Changement de dé : 1, 2, 3, 5, 6, 6' },
+        { id: 'item-2', name: 'Achille', image: 'assets/images/objects/poseidon-trident.jpg', description: 'Description 2' },
+        { id: 'item-3', name: 'Pandora', image: 'assets/images/objects/poseidon-trident.jpg', description: 'Description 3' },
+        { id: 'item-4', name: 'Zeus', image: 'assets/images/objects/poseidon-trident.jpg', description: 'Description 4' },
+        { id: 'item-5', name: 'Xiphos', image: 'assets/images/objects/poseidon-trident.jpg', description: 'Description 5' },
+        { id: 'item-6', name: 'Lyre', image: 'assets/images/objects/poseidon-trident.jpg', description: 'Description 6' },
+        { id: 'random-item', name: 'Random Item', image: 'assets/images/objects/poseidon-trident.jpg', description: 'Random item', count:2 },
+        { id: 'spawn-point', name: 'Point de départ', image: 'assets/images/objects/poseidon-trident.jpg', description: 'Spawn point', count: 2 }
+      ]; // temporary data structure
+
+    @Input() randomItemCount: number = 0;
+    @Input() spawnPointCount: number = 0;
+
+}
--- client/src/main.ts
+++ client/src/main.ts
@@ -6,6 +6,7 @@ import { Routes, provideRouter } from '@angular/router';
 import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { MapCreationPageComponent } from '@app/pages/map-creation-page/map-creation-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { environment } from './environments/environment';
 
@@ -13,9 +14,9 @@ if (environment.production) {
     enableProdMode();
 }
 
-
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'map', component: MapCreationPageComponent },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
@@ -24,4 +25,4 @@ const routes: Routes = [
 
 bootstrapApplication(AppComponent, {
     providers: [provideHttpClient(), provideRouter(routes), provideAnimations()],
-})
+});
