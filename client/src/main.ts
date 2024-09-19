88846d791a168e485f076c23a8aa14fc92a24741
GhadiFalouh <ghadif2001@hotmail.com>
2024-09-19 19:03:00 -0400

    feat(main): Add route for new pages

--- client/src/main.ts
+++ client/src/main.ts
@@ -9,6 +9,7 @@ import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MapCreationPageComponent } from '@app/pages/map-creation-page/map-creation-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { WaitingPageComponent } from '@app/pages/waiting-page/waiting-page.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -17,12 +18,12 @@ if (environment.production) {
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'create-game', component: CreateGamePageComponent },
+    { path: 'game-creation', component: CreateGamePageComponent },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'map', component: MapCreationPageComponent },
     { path: 'material', component: MaterialPageComponent },
-    { path: 'map', component: MapCreationPageComponent },
+    { path: 'waiting-page', component: WaitingPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
