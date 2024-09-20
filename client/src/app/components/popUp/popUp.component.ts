4c3a655a3d023d291037cd42797394c11fc29108
Kfor23 <kimia.foroudian@polymtl.ca>
2024-09-20 15:04:54 -0400

    feat(modal): Add the option to choose a button before going to the creation game page

--- client/src/app/components/popUp/popUp.component.ts
+++ client/src/app/components/popUp/popUp.component.ts
@@ -1,23 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-pop-up',
+    standalone: true,
     templateUrl: './popUp.component.html',
     styleUrls: ['./popUp.component.scss'],
+    imports: [NgClass, CommonModule],
 })
 export class PopUpComponent {
+    @Input() selectedSize: string;
+    @Input() selectedMode: string;
+
+    //gridArray: number[][];
     constructor(
         public dialogRef: MatDialogRef<PopUpComponent>,
         private router: Router,
     ) {}
+
+    /*ngOnChanges(changes: SimpleChanges) {
+        if (changes['selectedSize']) {
+            this.determineMapSize();
+            this.gridArray = this.createNewMap();
+        }
+        if (changes['resetTrigger'] && this.resetTrigger) {
+            this.resetGrid();
+        }
+    }*/
+
+    selectSize(size: string): void {
+        this.selectedSize = size;
+        console.log(size);
+    }
+
+    selectMode(mode: string): void {
+        this.selectedMode = mode;
+        console.log(mode);
+    }
+
     close(): void {
         this.dialogRef.close();
     }
 
     changePage(): void {
-        this.router.navigate(['/edit-map']);
+        this.router.navigate(['/edit-map'], { queryParams: { size: this.selectSize, mode: this.selectMode } });
         this.dialogRef.close();
     }
 }
