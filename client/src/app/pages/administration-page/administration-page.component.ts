f162b54f021c1e36633ecc2fb9c36134244cd3d9
GhadiFalouh <ghadif2001@hotmail.com>
2024-09-25 12:25:41 -0400

    feat(administration page): add redirection to pop-up component for game details

--- client/src/app/pages/administration-page/administration-page.component.ts
+++ client/src/app/pages/administration-page/administration-page.component.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { RouterLink } from '@angular/router';
 import { GameListComponent } from '@app/components/game-list/game-list.component';
+import { PopUpComponent } from '@app/components/popUp/popUp.component';
 
 @Component({
     selector: 'app-administration-page',
     standalone: true,
     templateUrl: './administration-page.component.html',
     styleUrls: ['./administration-page.component.scss'],
-    imports: [CommonModule, RouterLink, GameListComponent],
+    imports: [CommonModule, RouterLink, GameListComponent, MatDialogModule],
 })
-export class AdministrationPageComponent {}
+export class AdministrationPageComponent {
+    constructor(private dialog: MatDialog) {}
+    openPopUp(): void {
+        this.dialog.open(PopUpComponent, {
+            width: '400px',
+            height: '250px',
+        });
+    }
+}
