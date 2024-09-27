11a7d21fe9b402721124fff22bade9a17f1c0bc3
Kfor23 <kimia.foroudian@polymtl.ca>
2024-09-27 14:24:39 -0400

    feat(service): add methods to be able to use the size and mode of the map in the edition page

--- client/src/app/services/game-creation.service.ts
+++ client/src/app/services/game-creation.service.ts
@@ -5,11 +5,24 @@ import { BehaviorSubject } from 'rxjs';
     providedIn: 'root',
 })
 export class GameCreationService {
-    private sizeSubject = new BehaviorSubject<string | null>(null);
+    sizeSubject = new BehaviorSubject<string | null>(null);
+    modeSubject = new BehaviorSubject<string | null>(null);
 
     selectedSize$ = this.sizeSubject.asObservable();
 
     setSelectedSize(size: string) {
         this.sizeSubject.next(size);
+    }
+
+    setSelectedMode(mode: string) {
+        this.modeSubject.next(mode);
+    }
+
+    getSize() {
+        return this.sizeSubject.value;
+    }
+
+    getMode() {
+        return this.modeSubject.value;
     }
 }
