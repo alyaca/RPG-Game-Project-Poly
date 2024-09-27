c318deebfc44dcfcc5d6bd36ed8d1dd3efdb24c6
Kfor23 <kimia.foroudian@polymtl.ca>
2024-09-27 14:34:20 -0400

    test(game-creation-service): add test to cover all lines of the game-creation-service

--- client/src/app/services/game-creation.service.spec.ts
+++ client/src/app/services/game-creation.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GameCreationService } from '../../app/services/game-creation.service';
+import { GameCreationService } from '@app/services/game-creation.service';
 
 describe('GameCreationService', () => {
     let service: GameCreationService;
@@ -12,5 +12,23 @@ describe('GameCreationService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should select the right size', () => {
+        service.setSelectedSize('size');
+        expect(service.getSize()).toEqual('size');
+    });
+
+    it('should select the right mode', () => {
+        service.setSelectedMode('mode');
+        expect(service.getMode()).toEqual('mode');
+    });
+
+    it('should get the right size', () => {
+        expect(service.getSize()).toEqual(service.getSize());
+    });
+
+    it('should get the right mode', () => {
+        expect(service.getMode()).toEqual(service.getMode());
     });
 });
