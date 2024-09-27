6791e89723d0e6ccf6b7b092974695c4b2f9d6d2
Kfor23 <kimia.foroudian@polymtl.ca>
2024-09-27 14:18:02 -0400

    test(popUp): add test to reach full coverage for popUp component

--- client/src/app/components/popUp/popUp.component.spec.ts
+++ client/src/app/components/popUp/popUp.component.spec.ts
@@ -8,6 +8,7 @@ describe('PopUpComponent', () => {
     let fixture: ComponentFixture<PopUpComponent>;
     let dialogRefSpy: jasmine.SpyObj<MatDialogRef<PopUpComponent>>;
     let routerSpy: jasmine.SpyObj<Router>;
+    // let gameCreationServiceSpy: GameCreationService;
 
     beforeEach(async () => {
         dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
@@ -32,9 +33,40 @@ describe('PopUpComponent', () => {
         expect(component).toBeTruthy();
     });
 
+    it('should select the correct size of the map', () => {
+        const size = 'small';
+        component.selectSize(size);
+        expect(component.selectedSize).toBe(size);
+    });
+
+    it('should select the correct mode of the map', () => {
+        const mode = 'classic';
+        component.selectMode(mode);
+        expect(component.selectedMode).toBe(mode);
+    });
+
     it('should close the dialog when close() is called', () => {
         component.close();
         expect(dialogRefSpy.close).toHaveBeenCalled();
+    });
+
+    it('the subit button should be desabled when the mode and size is not selected', () => {
+        const result = component.isSubmitDisabled();
+        expect(result).toBe(true);
+    });
+
+    it('the subit button should be desabled when the size is not selected ', () => {
+        const mode = 'classic';
+        component.selectMode(mode);
+        const result = component.isSubmitDisabled();
+        expect(result).toBe(true);
+    });
+
+    it('the subit button should be desabled when the mode is not selected ', () => {
+        const size = 'small';
+        component.selectSize(size);
+        const result = component.isSubmitDisabled();
+        expect(result).toBe(true);
     });
 
     it('should navigate to the map creation page and close the dialog when changePage() is called', () => {
