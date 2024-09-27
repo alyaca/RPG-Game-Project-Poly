f003b38bf7ed25782666649af79146d8d4341a6e
Kfor23 <kimia.foroudian@polymtl.ca>
2024-09-27 14:30:01 -0400

    test(game-grid): add tests for size retrieval from service in game grid edition

--- client/src/app/components/edition-game-grid/edition-game-grid.component.spec.ts
+++ client/src/app/components/edition-game-grid/edition-game-grid.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { SIZE_LARGE_MAP, SIZE_MEDIUM_MAP, SIZE_SMALL_MAP } from '@app/constants';
 import { EditionGameGridComponent } from './edition-game-grid.component';
 
 describe('EditionGameGridComponent', () => {
@@ -27,6 +28,27 @@ describe('EditionGameGridComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should set map to small size values', () => {
+        component.selectedSize = 'small';
+        component.updateDimensions();
+        expect(component.height).toEqual(SIZE_SMALL_MAP);
+        expect(component.width).toEqual(SIZE_SMALL_MAP);
+    });
+
+    it('should set map to medium size values', () => {
+        component.selectedSize = 'medium';
+        component.updateDimensions();
+        expect(component.height).toEqual(SIZE_MEDIUM_MAP);
+        expect(component.width).toEqual(SIZE_MEDIUM_MAP);
+    });
+
+    it('should set map to large size values', () => {
+        component.selectedSize = 'large';
+        component.updateDimensions();
+        expect(component.height).toEqual(SIZE_LARGE_MAP);
+        expect(component.width).toEqual(SIZE_LARGE_MAP);
     });
 
     it('should have the correct number of tiles', () => {
