9335b57a867707f8a07f50b0106fa8b3021c4841
William Wang <williewang5632@gmail.com>
2024-09-27 16:29:17 -0400

    test(npm): ensuring test coverage is 100%

--- client/src/app/components/container-tools/container-tools.component.spec.ts
+++ client/src/app/components/container-tools/container-tools.component.spec.ts
@@ -1,22 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ContainerToolsComponent } from '@app/components/container-tools/container-tools.component';
+import { ToolService } from '@app/services/tool.service';
 
-import { ContainerToolsComponent } from './container-tools.component';
+class MockToolService {
+  selectedTile: string | null = null;
+
+  setSelectedTile(tile: string) {
+    this.selectedTile = tile;
+  }
+}
 
 describe('ContainerToolsComponent', () => {
-    let component: ContainerToolsComponent;
-    let fixture: ComponentFixture<ContainerToolsComponent>;
+  let component: ContainerToolsComponent;
+  let fixture: ComponentFixture<ContainerToolsComponent>;
+  let toolService: MockToolService;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [ContainerToolsComponent],
-        }).compileComponents();
-
-        fixture = TestBed.createComponent(ContainerToolsComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{ provide: ToolService, useClass: MockToolService }]
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+    fixture = TestBed.createComponent(ContainerToolsComponent);
+    component = fixture.componentInstance;
+    toolService = TestBed.inject(ToolService);
+  });
+
+  it('should set the selected tile when onSelectTile is called', () => {
+    const tile = 'exampleTile';
+
+    component.onSelectTile(tile);
+
+    expect(toolService.selectedTile).toBe(tile);
+  });
 });
