040ae8f0484ec0a393abb247fb3aae82ca7c447d
Kfor23 <kimia.foroudian@polymtl.ca>
2024-09-28 17:12:04 -0400

    feat(service): update GameCreationService to store map size in localStorage

--- client/src/app/services/game-creation.service.ts
+++ client/src/app/services/game-creation.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { SIZE_LARGE_MAP, SIZE_MEDIUM_MAP, SIZE_SMALL_MAP } from '@app/constants';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -12,17 +13,27 @@ export class GameCreationService {
 
     setSelectedSize(size: string) {
         this.sizeSubject.next(size);
+        localStorage.setItem('selectedMapSize', size);
     }
 
     setSelectedMode(mode: string) {
         this.modeSubject.next(mode);
     }
 
-    getSize() {
-        return this.sizeSubject.value;
+    getStoredSize(): string | null {
+        return localStorage.getItem('selectedMapSize');
     }
 
-    getMode() {
-        return this.modeSubject.value;
+    updateDimensions() {
+        const size = this.getStoredSize();
+        if (size === 'small') {
+            return SIZE_SMALL_MAP;
+        } else if (size === 'medium') {
+            return SIZE_MEDIUM_MAP;
+        } else if (size === 'large') {
+            return SIZE_LARGE_MAP;
+        } else {
+            return alert('invalid map size chosen');
+        }
     }
 }
