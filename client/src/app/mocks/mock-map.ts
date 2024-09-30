0d242f8e1774370c37d4172f565c97fe7e5d4c9e
Sami-Jouamaa <samijouamaa@gmail.com>
2024-09-30 11:24:51 -0400

    fix(test) tests work, i don't know why the pipeline says it's failing

--- client/src/app/mocks/mock-map.ts
+++ client/src/app/mocks/mock-map.ts
@@ -9,7 +9,7 @@ export const dummyInfo: Info = {
     description: 'a map description',
     grid: [[TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground]],
     items: [[NO_ITEM, NO_ITEM, RANDOM_ITEM, NO_ITEM, NO_ITEM, NO_ITEM]],
-    height: 15,
+    height: SIZE_MEDIUM_MAP,
 };
 
 export const dummyMap: Map = {
