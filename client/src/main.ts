aaa56f714a59f2c98df9244b90b4fbdf6763bac4
Kfor23 <kimia.foroudian@polymtl.ca>
2024-10-01 14:55:36 -0400

    chore(unused-files): delete all initial files that are not used in the projet

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,7 +6,6 @@ import { Routes, provideRouter } from '@angular/router';
 import { AdministrationPageComponent } from '@app/pages/administration-page/administration-page.component';
 import { AppComponent } from '@app/pages/app/app.component';
 import { CreateGamePageComponent } from '@app/pages/create-game-page/create-game-page.component';
-import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MapCreationPageComponent } from '@app/pages/map-creation-page/map-creation-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
@@ -23,7 +22,6 @@ const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'administration', component: AdministrationPageComponent },
     { path: 'edit-map', component: MapCreationPageComponent},
-    { path: 'game', component: GamePageComponent },
     { path: 'map', component: MapCreationPageComponent },
     { path: 'material', component: MaterialPageComponent },
     { path: 'waiting-page', component: WaitingPageComponent },
