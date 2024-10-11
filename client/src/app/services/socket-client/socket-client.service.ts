7753e1044c353188230e2493ee7494a753e71f29
KimDesroches <kim.desroches@polymtl.ca>
2024-10-11 12:10:24 -0400

    feature(socket): add socketIO example in client

---
+++ client/src/app/services/socket-client/socket-client.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Socket, io } from 'socket.io-client';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SocketClientService {
+    socket: Socket;
+
+    isSocketAlive() {
+        return this.socket && this.socket.connected;
+    }
+
+    connect() {
+        if (!this.isSocketAlive()) {
+            this.socket = io(environment.serverUrl, { transports: ['websocket'], upgrade: false });
+        }
+    }
+
+    disconnect() {
+        this.socket.disconnect();
+    }
+
+    on<T>(event: string, action: (data: T) => void): void {
+        this.socket.on(event, action);
+    }
+
+    send<T>(event: string, data?: T, callback?: Function): void {
+        this.socket.emit(event, ...[data, callback].filter((x) => x));
+    }
+}
