6ca32772eac81a0b36d753093e5f39e1535a36da
Kfor23 <kimia.foroudian@polymtl.ca>
2024-10-11 21:49:05 -0400

    test(join-game): update tests to reach full coverage

--- client/src/app/pages/join-game/join-game.component.spec.ts
+++ client/src/app/pages/join-game/join-game.component.spec.ts
@@ -2,7 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { JoinGameComponent } from './join-game.component';
 
-
 describe('JoinGameComponent', () => {
     let component: JoinGameComponent;
     let fixture: ComponentFixture<JoinGameComponent>;
@@ -10,14 +9,12 @@ describe('JoinGameComponent', () => {
     let activatedRouteMock: jasmine.SpyObj<ActivatedRoute>;
 
     beforeEach(async () => {
-      
-
         await TestBed.configureTestingModule({
             imports: [JoinGameComponent],
-            providers: [ { provide: Router, useValue: routerMock },
-              { provide: ActivatedRoute, useValue: activatedRouteMock },
+            providers: [
+                { provide: Router, useValue: routerMock },
+                { provide: ActivatedRoute, useValue: activatedRouteMock },
             ],
-
         }).compileComponents();
 
         fixture = TestBed.createComponent(JoinGameComponent);
@@ -29,46 +26,40 @@ describe('JoinGameComponent', () => {
         expect(component).toBeTruthy();
     });
 
-   it(('should return true when roomExists is called with a gode that exists'), () => {
+    it('should return true when roomExists is called with a gode that exists', () => {
         component.fakeCode = '1234';
         component.accessCode = '1234';
         expect(component.roomExists(component.accessCode)).toBe(true);
     });
 
-    
-   it(('should return true when roomExists is called with a code that exists'), () => {
-    component.fakeCode = '1234';
-    component.accessCode = '1234';
-    expect(component.roomExists(component.accessCode)).toBe(true);
+    it('should return true when roomExists is called with a code that exists', () => {
+        component.fakeCode = '1234';
+        component.accessCode = '1234';
+        expect(component.roomExists(component.accessCode)).toBe(true);
     });
 
-    it(('should return false when roomExists is called with a code that does not exist'), () => {
-      component.fakeCode = '1234';
-      component.accessCode = '1784';
-      expect(component.roomExists(component.accessCode)).toBe(false);
+    it('should return false when roomExists is called with a code that does not exist', () => {
+        component.fakeCode = '1234';
+        component.accessCode = '1784';
+        expect(component.roomExists(component.accessCode)).toBe(false);
     });
 
-    
     it('should set isCharacterFormVisible to false when hideCharacterForm is called', () => {
-      component.hideCharacterForm();
-      expect(component.isCharacterFormVisible).toBeFalse();
+        component.hideCharacterForm();
+        expect(component.isCharacterFormVisible).toBeFalse();
     });
 
-    it(('should set isCharacterFormVisible to true when joinGame is called with a code that exists'), () => {
-      component.accessCode = '7654';
-      component.fakeCode = '7654';
-      component.joinGame(component.accessCode);
-      expect(component.isCharacterFormVisible).toBeTrue();
+    it('should set isCharacterFormVisible to true when joinGame is called with a code that exists', () => {
+        component.accessCode = '7654';
+        component.fakeCode = '7654';
+        component.joinGame(component.accessCode);
+        expect(component.isCharacterFormVisible).toBeTrue();
     });
 
-    
-    it(('should not set isCharacterFormVisible to true when joinGame is called with a code that does not exist'), () => {
-      component.accessCode = '7654';
-      component.fakeCode = '1111';
-      component.joinGame(component.accessCode);
-      expect(component.isCharacterFormVisible).toBeFalse();
+    it('should not set isCharacterFormVisible to true when joinGame is called with a code that does not exist', () => {
+        component.accessCode = '7654';
+        component.fakeCode = '1111';
+        component.joinGame(component.accessCode);
+        expect(component.isCharacterFormVisible).toBeFalse();
     });
-
-
-
 });
