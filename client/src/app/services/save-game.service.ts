c3ac038810dbf1b3de31b1a5d20ac9ad806051e0
GhadiFalouh <ghadif2001@hotmail.com>
2024-10-12 03:23:02 -0400

    Refactor (saveGameService): improve code quality and simplify logic

--- client/src/app/services/save-game.service.ts
+++ client/src/app/services/save-game.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NB_ITEMS_LARGE_MAP, NB_ITEMS_MEDIUM_MAP, NB_ITEMS_SMALL_MAP, SIZE_LARGE_MAP, SIZE_MEDIUM_MAP, SIZE_SMALL_MAP } from '@app/constants';
 import { Info } from '@app/interfaces/info';
-import { Map } from '@app/interfaces/map';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -13,55 +12,38 @@ export class SaveGameService {
 
     constructor(private http: HttpClient) {}
 
-    // Some information will have to be retrieved from the selectedMap coming from admin
-    saveGame(informations: Info, selectedMap: Map | null) {
-        if (selectedMap == null) {
-            let playerNumber;
-            switch (informations.height) {
-                case SIZE_SMALL_MAP: {
-                    playerNumber = NB_ITEMS_SMALL_MAP;
-                    break;
-                }
-                case SIZE_MEDIUM_MAP: {
-                    playerNumber = NB_ITEMS_MEDIUM_MAP;
-                    break;
-                }
-                case SIZE_LARGE_MAP: {
-                    playerNumber = NB_ITEMS_LARGE_MAP;
-                    break;
-                }
-            }
+    saveGame(informations: Info) {
+        const playerNumber = this.getPlayerNumber(informations.height);
+        const mapToStore = this.createMapObject(informations, playerNumber);
+        return this.http.post(this.apiURL, mapToStore).subscribe();
+    }
 
-            const mapToStore = {
-                name: informations.name,
-                description: informations.description,
-                visible: false,
-                mode: 'normal',
-                nbPlayers: playerNumber,
-                image: informations.image,
-                tiles: informations.grid,
-                dimension: informations.height,
-                itemPlacement: informations.items,
-                isSelected: false,
-                lastModification: new Date(),
-            };
-            return this.http.post(this.apiURL, mapToStore).subscribe();
-        } else {
-            const mapToReplace = {
-                _id: selectedMap._id,
-                name: informations.name,
-                description: informations.description,
-                visible: false,
-                mode: selectedMap.mode,
-                nbPlayers: selectedMap.nbPlayers,
-                image: informations.image,
-                tiles: informations.grid,
-                dimension: selectedMap.dimension,
-                itemPlacement: informations.items,
-                isSelected: false,
-                lastModification: new Date(),
-            };
-            return this.http.put(this.apiURL, mapToReplace).subscribe();
+    private getPlayerNumber(height: number): number {
+        switch (height) {
+            case SIZE_SMALL_MAP:
+                return NB_ITEMS_SMALL_MAP;
+            case SIZE_MEDIUM_MAP:
+                return NB_ITEMS_MEDIUM_MAP;
+            case SIZE_LARGE_MAP:
+                return NB_ITEMS_LARGE_MAP;
+            default:
+                throw new Error('Taille de carte invalide');
         }
+    }
+
+    private createMapObject(informations: Info, playerNumber: number): any {
+        return {
+            name: informations.name,
+            description: informations.description,
+            visible: false,
+            mode: 'normal',
+            nbPlayers: playerNumber,
+            image: informations.image,
+            tiles: informations.grid,
+            dimension: informations.height,
+            itemPlacement: informations.items,
+            isSelected: false,
+            lastModification: new Date(),
+        };
     }
 }
