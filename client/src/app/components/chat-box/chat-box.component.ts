a5e0953bc22432195294bb02f4e86f165f5d6818
Ali Yacoubi <ali.yacoubi@polymtl.ca>
2024-10-25 13:33:57 -0400

    fix(lint): fix lint in chat box component

--- client/src/app/components/chat-box/chat-box.component.ts
+++ client/src/app/components/chat-box/chat-box.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ChatMessageComponent } from '@app/components/chat-message/chat-message.component';
 import { ChatMessage } from '@app/interfaces/chat-message';
@@ -12,20 +12,19 @@ import { ChatService } from '@app/services/sockets/chat/chat.service';
     templateUrl: './chat-box.component.html',
     styleUrl: './chat-box.component.scss',
 })
-export class ChatBoxComponent implements OnInit {
+export class ChatBoxComponent implements OnInit, AfterViewChecked {
+    @ViewChild('messageContainer') messageContainer: ElementRef<HTMLDivElement>;
     messages: ChatMessage[] = [];
     newMessage: string = '';
     isChatVisible: boolean = true;
 
-    @ViewChild('messageContainer') public messageContainer: ElementRef<HTMLDivElement>;
+    constructor(private chatService: ChatService) {}
 
     scrollToBottom(): void {
         if (this.messageContainer) {
             this.messageContainer.nativeElement.scrollTop = this.messageContainer.nativeElement.scrollHeight;
         }
     }
-
-    constructor(private chatService: ChatService) {}
 
     ngOnInit(): void {
         this.chatService.onMessageReceived((message: ChatMessage) => {
