dccadaa538adf6701884090297c9480a2d7376ff
Sami-Jouamaa <samijouamaa@gmail.com>
2024-11-01 17:35:18 -0400

    fix(links)

--- client/src/app/pages/game-page/game-page.component.spec.ts
+++ client/src/app/pages/game-page/game-page.component.spec.ts
@@ -7,7 +7,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { SimpleDialogComponent } from '@app/components/simple-dialog/simple-dialog.component';
 import { SocketCommunicationService } from '@app/services/sockets/socket-communication/socket-communication.service';
 import { of } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { GamePageComponent } from './game-page.component';
 
 describe('GamePageComponent', () => {
--- client/src/app/pages/waiting-page/waiting-page.component.spec.ts
+++ client/src/app/pages/waiting-page/waiting-page.component.spec.ts
@@ -13,7 +13,7 @@ import { GameService } from '@app/services/sockets/game/game.service';
 import { SocketCommunicationService } from '@app/services/sockets/socket-communication/socket-communication.service';
 import { Game } from '@common/game';
 import { BehaviorSubject, of } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { WaitingPageComponent } from './waiting-page.component';
 
 describe('WaitingPageComponent', () => {
