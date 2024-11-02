1173ab8668ae0ea6387e4524729b84d9422f7766
William Wang <williewang5632@gmail.com>
2024-11-02 17:12:10 -0400

    test(dice): attempting fix pipeline failing test

--- client/src/app/components/dice/dice.component.spec.ts
+++ client/src/app/components/dice/dice.component.spec.ts
@@ -1,7 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { DiceComponent } from './dice.component';
-// import { INACTIVE_DICE_DELAY, ROLL_DURATION } from '@app/constants';
-import { ROLL_DURATION } from '@app/constants'; // temporary
+import { INACTIVE_DICE_DELAY, ROLL_DURATION } from '@app/constants';
 
 describe('DiceComponent', () => {
     let component: DiceComponent;
@@ -38,18 +37,18 @@ describe('DiceComponent', () => {
         }, ROLL_DURATION);
     });
 
-    // it('should not roll if already rolling', () => {
-    //     const maxValue = 6;
-    //     component.isRolling = true;
+    it('should not roll if already rolling', fakeAsync(() => {
+        const maxValue = 6;
+        component.isRolling = true;
 
-    //     component.rollDice(maxValue);
+        component.rollDice(maxValue);
 
-    //     const previousValue = component.value;
-    //     setTimeout(() => {
-    //         expect(component.value).toBe(previousValue);
-    //         expect(component.isRolling).toBe(true);
-    //     }, INACTIVE_DICE_DELAY);
-    // });
+        const previousValue = component.value;
+        tick(INACTIVE_DICE_DELAY); // Advance time by the inactive dice delay
+
+        expect(component.value).toBe(previousValue);
+        expect(component.isRolling).toBe(true);
+    }));
 
     it('should allow rolling again after completing the first roll', (done) => {
         const maxValue = 6;
