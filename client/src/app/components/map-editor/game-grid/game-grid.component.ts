82be4520c4e7cb1efdb9df48542f4e69c44b42ed
William Wang <williewang5632@gmail.com>
2024-11-29 12:30:54 -0500

    fix(lint): fixing lint errors

--- client/src/app/components/map-editor/game-grid/game-grid.component.ts
+++ client/src/app/components/map-editor/game-grid/game-grid.component.ts
@@ -19,7 +19,7 @@ import { ValidatingMapInfo } from '@app/interfaces/validating-map-info';
 import { GameCreationService } from '@app/services/game-creation/game-creation.service';
 import { GameObjectService } from '@app/services/game-object/game-object.service';
 import { GameTileInfoService } from '@app/services/game-tile-info/game-tile-info.service';
-import { TileType } from '@common/constants'
+import { TileType } from '@common/constants';
 import { MapValidatorService } from '@app/services/map-validator/map-validator.service';
 import { NavigationService } from '@app/services/navigation/navigation.service';
 import { GameService } from '@app/services/sockets/game/game.service';
