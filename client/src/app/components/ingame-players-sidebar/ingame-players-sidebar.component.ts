3fa6cfdc51e117f63dc821ffb426ba63cc445199
William Wang <williewang5632@gmail.com>
2024-11-29 19:36:08 -0500

    fix(lint): fixing lint errors

--- client/src/app/components/ingame-players-sidebar/ingame-players-sidebar.component.ts
+++ client/src/app/components/ingame-players-sidebar/ingame-players-sidebar.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { SocketCommunicationService } from '@app/services/sockets/socket-communication/socket-communication.service';
 import { ObjectType } from '@common/constants';
 import { Player } from '@common/interfaces/player';
 
@@ -13,7 +12,6 @@ import { Player } from '@common/interfaces/player';
 })
 export class IngamePlayersSidebarComponent {
     @Input() sidebarPlayer: Player;
-    constructor(public socketCommunicationService: SocketCommunicationService){}
 
     isFlagInInventory() {
         return this.sidebarPlayer.inventory.find((item) => item.id === ObjectType.Flag);
