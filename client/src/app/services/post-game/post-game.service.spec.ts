7a073a98e9bc846da189d6f62396b1129639b70f
William Wang <williewang5632@gmail.com>
2024-12-02 00:11:51 -0500

    fix(post-game): I forgor to comment forceEndGame, silly me!!!

--- client/src/app/services/post-game/post-game.service.spec.ts
+++ client/src/app/services/post-game/post-game.service.spec.ts
@@ -351,7 +351,7 @@ describe('PostGameService', () => {
     });
 
     describe('calculateFlagBearers', () => {
-        it('should correctly count players with ObjectType.Kunee items', () => {
+        it('should correctly count players with ObjectType.Flag items', () => {
             service['players'] = [
                 { collectedItems: [ObjectType.Lightning, ObjectType.Flag], postGameStats: {} } as Player,
                 { collectedItems: [ObjectType.Trident, ObjectType.Flag], postGameStats: {} } as Player,
